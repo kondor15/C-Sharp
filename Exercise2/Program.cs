9d7cb3a0e3caee464ea1e5a55616fec8d6df091d
kondor15 <33878509+kondor15@users.noreply.github.com>
2017-11-27 14:37:26 +0200

    Add files via upload

---
+++ Exercise2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Program
+    {
+      public static void Main(string[] args)
+        {
+            var q = new Queue();
+            string name = "dfs";
+            while (name !=string.Empty)
+            {
+                name = Console.ReadLine();
+                q.Push(name);
+            }
+
+            while (!q.Empty)
+            {
+                string names = q.Pop();
+                Console.WriteLine(names);
+            }
+            Console.ReadLine();
+        }
+        
+        
+        
+        
+    }
+}
---
+++ Exercise2/Queue.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    public class Queue
+    {
+        private List<string> container;
+        public Queue()
+        {
+            this.container = new List<string>();
+        }
+        public void Push(string val)
+        { 
+            this.container.Add(val);
+        }
+        public string Pop()
+        {
+            string popped = container[0];
+            container.RemoveAt(0);
+            return popped;
+        }
+        public bool Empty
+        {
+            get
+            {
+                return container.Count == 0;
+            }
+        }
+    }
+}
